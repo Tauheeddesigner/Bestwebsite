d35dc70093da5d59a97938201a241f2b3918b9cb
Tauheeddeveloper <tauheedbhabha@gmail.com>
2024-11-11 11:37:30 +0500

    add some feature

--- contact.js
+++ contact.js
@@ -4,25 +4,42 @@
 //     backSpeed:80,
 //     loop:true
 //   });
-let tl=gsap.timeline()
-tl.from("h1",{
-  y:-250,
-  opacity:0,
-  duration:1,
+// let tl=gsap.timeline()
+// tl.from("h1",{
+//   y:-250,
+//   opacity:0,
+//   duration:1,
  
 
 
-})
-tl.from(".box",{
-  y:-300,
-  opacity:0,
-  duration1,
-})
+// })
+// tl.from(".box",{
+//   y:-300,
+//   opacity:0,
+//   duration1,
+// })
 
-tl.from(".information",{
-    y:300,
-    opacity:0,
-    duration:1,
+// tl.from(".information",{
+//     y:300,
+//     opacity:0,
+//     duration:1,
   
     
-})
+// })
+// JavaScript to animate and add effects on page load
+window.onload = function() {
+  // Example of animating the background color change for the page
+  document.body.style.transition = "background-color 2s ease-in-out";
+  document.body.style.backgroundColor = "black";
+
+  // Add typing animation using Typed.js for a dynamic effect
+  const typed = new Typed("h1", {
+      strings: ["Contact me for more information!", "Feel free to reach out!", "Let's connect on LinkedIn!"],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1500,
+      startDelay: 500,
+      loop: true
+  });
+}
+
